34f0bee08be1b1b17b4dc46e1a7b5507c84c8f19
Nick Jacobson <33440907+nick-jac@users.noreply.github.com>
2017-12-12 15:07:17 +1300

    added orange color change

--- events.js
+++ events.js
@@ -8,6 +8,7 @@ function start () {
   // when you mouse over it.
   one()
   two()
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -29,7 +30,11 @@ function two () {
   two.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.addEventListener('mouseenter', makeOrange)
+  three.addEventListener('mouseleave', makeWhite)
+}
 
 // CREATE FUNCTION four HERE
 
@@ -44,6 +49,14 @@ function makeWhite (evt) {
 
 function makeGreen (evt) {
   evt.target.style.backgroundColor = 'green'
+}
+
+function makeWhite (evt) {
+  evt.target.style.backgroundColor = 'white'
+}
+
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
 }
 
 function makeWhite (evt) {
